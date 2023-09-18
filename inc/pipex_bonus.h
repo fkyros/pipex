3cc9de033781b883aa975bb73a0e09e32fc8405c
gabri <gade-oli@student.42madrid.com>
2023-09-18 22:28:54 +0200

    first approach to bonus

---
+++ inc/pipex_bonus.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gade-oli <gade-oli@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 21:10:13 by gade-oli          #+#    #+#             */
+/*   Updated: 2023/09/18 22:28:25 by gade-oli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_BONUS_H
+# define PIPEX_BONUS_H
+
+# define BONUS 1
+
+# include "../libft/libft.h"
+# include <unistd.h>
+# include <fcntl.h>
+
+# define SUCCESS 0
+# define FAIL 1
+
+#endif
